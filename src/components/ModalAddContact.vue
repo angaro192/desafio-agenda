1cdde6dc18db1f2ae8ecd6e77f9c9a361ea5bc99
Augusto Almeida <augustalmeid@gmail.com>
2025-02-09 22:50:50 -0300

    Melhoria dos itens no modal de contact e uso do VIACEP no axios, para exemplos

--- src/components/ModalAddContact.vue
+++ src/components/ModalAddContact.vue
@@ -1,6 +1,15 @@
 <script setup lang="ts">
+import axios from "axios";
 import { Button, Dialog, InputText } from "primevue";
-import { computed } from "vue";
+import { computed, ref, watch } from "vue";
+
+export type ContactFormType = {
+  name: string;
+  email: string;
+  contact: string;
+  zipCode: string;
+  address: string;
+};
 
 const props = defineProps<{
   visible: boolean;
@@ -16,6 +25,37 @@ const statusModal = computed({
 const emit = defineEmits<{
   (e: "getVisible", value: boolean): void;
 }>();
+
+const initialValue = ref<ContactFormType>({
+  name: "",
+  email: "",
+  contact: "",
+  zipCode: "",
+  address: "",
+});
+
+const handleZipCodeChange = (contactValue: ContactFormType) => {
+  if (contactValue.zipCode.length >= 9) {
+    const zipCodeFormated = contactValue.zipCode.replace("-", "");
+    axios
+      .get(`https://viacep.com.br/ws/${zipCodeFormated}/json/`)
+      .then((result) => {
+        if (result.status === 200) {
+          initialValue.value.address = `${result.data.logradouro}, ${
+            result.data.localidade
+          }, ${result.data.uf.toUpperCase()}`;
+        }
+      });
+  }
+};
+
+watch(
+  initialValue,
+  (contactValue: ContactFormType) => {
+    handleZipCodeChange(contactValue);
+  },
+  { deep: true }
+);
 </script>
 
 <template>
@@ -25,34 +65,60 @@ const emit = defineEmits<{
     header="Novo contato"
     :style="{ width: '25rem' }"
   >
-    <div class="flex items-center gap-4 mb-4">
-      <label for="username" class="font-semibold w-24">Nome</label>
-      <InputText id="username" class="flex-auto" autocomplete="off" />
-    </div>
-    <div class="flex items-center gap-4 mb-8">
-      <label for="email" class="font-semibold w-24">Email</label>
-      <InputText id="email" class="flex-auto" autocomplete="off" />
-    </div>
-    <div class="flex items-center gap-4 mb-8">
-      <label for="phoneNumber" class="font-semibold w-24">Contato</label>
-      <InputText id="phoneNumber" class="flex-auto" autocomplete="off" />
-    </div>
-    <div class="flex items-center gap-4 mb-8">
-      <label for="address" class="font-semibold w-24">Endereço</label>
-      <InputText id="address" class="flex-auto" autocomplete="off" />
-    </div>
-    <div class="flex justify-end gap-2">
-      <Button
-        type="button"
-        label="Cancelar"
-        severity="secondary"
-        @click="emit('getVisible', false)"
-      ></Button>
-      <Button
-        type="button"
-        label="Salvar"
-        @click="emit('getVisible', false)"
-      ></Button>
-    </div>
+    <Form :initialValues="initialValue">
+      <div class="flex items-center gap-4 mb-4">
+        <label for="name" class="font-semibold w-24">Nome</label>
+        <InputText
+          id="name"
+          v-model="initialValue.name"
+          class="flex-auto"
+          autocomplete="off"
+        />
+      </div>
+      <div class="flex items-center gap-4 mb-8">
+        <label for="email" class="font-semibold w-24">Email</label>
+        <InputText
+          id="email"
+          v-model="initialValue.email"
+          class="flex-auto"
+          autocomplete="off"
+        />
+      </div>
+      <div class="flex items-center gap-4 mb-8">
+        <label for="contact" class="font-semibold w-24">Contato</label>
+        <InputText
+          id="contact"
+          v-model="initialValue.contact"
+          class="flex-auto"
+          autocomplete="off"
+        />
+      </div>
+      <div class="flex items-center gap-4 mb-2">
+        <label for="zipCode" class="font-semibold w-24">CEP</label>
+        <InputText
+          id="zipCode"
+          v-model="initialValue.zipCode"
+          v-mask="'#####-###'"
+          class="flex-auto"
+          autocomplete="off"
+        />
+      </div>
+      <div class="flex justify-end gap-2 mb-8">
+        <p v-if="initialValue.address">{{ initialValue.address }}</p>
+      </div>
+      <div class="flex justify-end gap-2">
+        <Button
+          type="button"
+          label="Cancelar"
+          severity="secondary"
+          @click="emit('getVisible', false)"
+        ></Button>
+        <Button
+          type="button"
+          label="Salvar"
+          @click="emit('getVisible', false)"
+        ></Button>
+      </div>
+    </Form>
   </Dialog>
 </template>
